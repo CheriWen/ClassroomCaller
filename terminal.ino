a0c7023138c53edd553cdb9ae3943c41c4c1abce
Cheri Wen <105411647+CheriWen@users.noreply.github.com>
2024-10-23 15:55:27 +0800

    Terminal.ino

---
+++ terminal.ino
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+// Initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+    Serial.begin(9600);
+    lcd.begin(16, 2);
+    lcd.print("Happy Study!");
+}
+
+void loop() {
+    if (Serial.avalable()){
+        String data = Serial.readStringUntil('\n');
+        Serial.flush();
+
+        //Clear the LCD
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Please call:");
+    }
+}
